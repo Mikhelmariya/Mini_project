cd4b4f907acfcbeb3cf265daa95b751c323b8f35
Mikhelmariya <mikhela65@gmail.com>
2023-03-15 00:34:26 +0530

    messages

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ app.post("/webhooks",(req,res)=>{
     if(body_param.object){
         if(body_param.entry && 
             body_param.entry[0].changes[0]&&
-             body_param.entry[0].changes[0].value.message && 
-             body_param.entry[0].changes[0].value.message[0]){
+             body_param.entry[0].changes[0].value.messages && 
+             body_param.entry[0].changes[0].value.messages[0]){
                
                 let phone_no_id = body_param.entry[0].changes[0].values.metadata.phone_number_id;
                 let from =body_param.entry[0].changes[0].value.messages[0].from;
