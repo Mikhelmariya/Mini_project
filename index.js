1f1eadfbd116c159ad84bbe573f1f9142e0f3797
Mikhelmariya <mikhela65@gmail.com>
2023-04-17 18:57:35 +0530

    new token

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ require('dotenv').config();
  // const tocken ="EAAKl03DggZBYBAI6kMrIzCqh7HDeNEdj22waZA7dweyPItOeLcnN43Kw1CyTyZAk9zZBLdnT3sZALBqiugPEnTBc6WPZBl6JqwCQ2t9R30DBBzKsxitZCiLfgnumvkHNYy6kmD2iBdM7JzatN3zVt2XHEQ921kcXDZBOheZC9lZBVfyYug5QhAod3A"
 
 //const tocken="EAAKl03DggZBYBAMFfqzAmaYlDKUwy2XF6wpYlcYjxR4QNOInI6blnNh9ihLoWeZCp3ErYqmJPB3xMwKWD57z3FFjYsuOFqftt7Cbs8JJabfHgZAKrL6bEfZCIGD8M8wKam9GGIyKw3GMMZBjTGjMt63ctZBf8JapDxawpd7Hi7uq3yvvyNbGqQQhRXvVpDNIWiqlBuc7WEQQZDZD"
-const tocken="EAAKl03DggZBYBAMFfqzAmaYlDKUwy2XF6wpYlcYjxR4QNOInI6blnNh9ihLoWeZCp3ErYqmJPB3xMwKWD57z3FFjYsuOFqftt7Cbs8JJabfHgZAKrL6bEfZCIGD8M8wKam9GGIyKw3GMMZBjTGjMt63ctZBf8JapDxawpd7Hi7uq3yvvyNbGqQQhRXvVpDNIWiqlBuc7WEQQZDZD"
+//const tocken="EAAKl03DggZBYBAMFfqzAmaYlDKUwy2XF6wpYlcYjxR4QNOInI6blnNh9ihLoWeZCp3ErYqmJPB3xMwKWD57z3FFjYsuOFqftt7Cbs8JJabfHgZAKrL6bEfZCIGD8M8wKam9GGIyKw3GMMZBjTGjMt63ctZBf8JapDxawpd7Hi7uq3yvvyNbGqQQhRXvVpDNIWiqlBuc7WEQQZDZD"
+const tocken ="EAAKl03DggZBYBACzsCDSV6LBW5BAAfV2LryfUQ1Yv0PSu1xmDZCXKBZBaZAIXuUlG2PI3CMj8bzun8h0x1OCpIs7HwTNZCHJSHrI8pjfDNBMI6wC8qQZBSwg2fSxHBBIDIiNSAoZCL4VkLkmh55wWmOWjH9fZAgeqJu0KqRqOS7atEFdCTsBUQINQJvWC1OCJHcIF5viROXXxAZDZD"
 app.get("/webhook",(req,res)=>{
 let mode= req.query["hub.mode"];
 let challange= req.query["hub.challenge"];
@@ -133,7 +134,7 @@ app.post("/webhook",(req,res)=>{
           
           headers: { 
               'Content-Type': 'application/json', 
-              'Authorization': 'Bearer EAAKl03DggZBYBAMFfqzAmaYlDKUwy2XF6wpYlcYjxR4QNOInI6blnNh9ihLoWeZCp3ErYqmJPB3xMwKWD57z3FFjYsuOFqftt7Cbs8JJabfHgZAKrL6bEfZCIGD8M8wKam9GGIyKw3GMMZBjTGjMt63ctZBf8JapDxawpd7Hi7uq3yvvyNbGqQQhRXvVpDNIWiqlBuc7WEQQZDZD'
+              'Authorization': 'Bearer EAAKl03DggZBYBACzsCDSV6LBW5BAAfV2LryfUQ1Yv0PSu1xmDZCXKBZBaZAIXuUlG2PI3CMj8bzun8h0x1OCpIs7HwTNZCHJSHrI8pjfDNBMI6wC8qQZBSwg2fSxHBBIDIiNSAoZCL4VkLkmh55wWmOWjH9fZAgeqJu0KqRqOS7atEFdCTsBUQINQJvWC1OCJHcIF5viROXXxAZDZD'
             },
             data : data
           };
