c3a67d18954eeac290872a5c0fc4c423cf548d2b
Mikhelmariya <mikhela65@gmail.com>
2023-05-30 12:46:26 +0530

    m

--- index.js
+++ index.js
@@ -146,11 +146,8 @@ app.post("/webhook",(req,res)=>{
         
       } 
     if(msg_body=="Hii"){
-      console.log("inside if msg_body");
       module.exports.name(phone_no_id, tocken, from, res);
-      
-
-  }
+    }
     // else if(msg_body=="Hello"){
     //   console.log("inside else msg_body");
     //   module.exports.custom(phone_no_id, tocken, from, res);
