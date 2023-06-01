f673dfd26d2f4c40bf344b08999f9b275bc1f2b5
Mikhelmariya <mikhela65@gmail.com>
2023-06-01 22:57:49 +0530

    mm

--- index.js
+++ index.js
@@ -93,12 +93,12 @@ app.post("/webhook",(req,res)=>{
                       {
                         "id": "<LIST_SECTION_1_ROW_1_ID>",
                         "title": "Coding club",
-                        "description": "Coding Club TKMCE is a vibrant community of passionate programmers at TKM College of Engineering, where members collaborate, learn, and innovate together, creating a dynamic space for coding enthusiasts to thrive."
+                        "description": "Coding Club TKMCE is a community of passionate programmers at TKM College of Engineering, where members collaborate, learn, and innovate together, creating a dynamic space for coding enthusiasts to thrive."
                       },
                       {
                         "id": "<LIST_SECTION_1_ROW_2_ID>",
                         "title": "IEDC",
-                        "description": ""
+                        "description": "IEDC TKMCE is a platform at TKM College of Engineering that nurtures entrepreneurship by providing students with mentorship, workshops, and networking opportunities to turn their ideas into successful ventures."
                       }
                     ]
                   },
