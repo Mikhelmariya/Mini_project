eb593c1c4a70f36d3040ca4cdc12b66b98e48f94
Mikhelmariya <mikhela65@gmail.com>
2023-06-06 18:25:51 +0530

    spell

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const body_parser = require("body-parser");
 const axios = require("axios");
-const { getOpenai } = require("./controller"); // Replace "./controller" with the actual path to your controller.js file
+const { getOpenai } = require("./contoller"); // Replace "./controller" with the actual path to your controller.js file
 
 
 const port=process.env.PORT || 8000;
