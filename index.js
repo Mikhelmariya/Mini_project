21a773464a4b0a5a69d29e963aa42be7f2401a38
Mikhelmariya <mikhela65@gmail.com>
2023-06-06 18:45:59 +0530

    b

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const body_parser = require("body-parser");
 const axios = require("axios");
 //const { getOpenai } = require("./contoller"); // Replace "./controller" with the actual path to your controller.js file
-const { getOpenai } = require("./contoller.js");
+const  getOpenai  = require("./contoller.js");
 
 const port=process.env.PORT || 8000;
 const app=express().use(body_parser.json());
