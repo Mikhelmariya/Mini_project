04f9ae921c01a523b57245f7be76a6c4316b655a
Mikhelmariya <mikhela65@gmail.com>
2023-06-06 10:52:40 +0530

    TOUGH

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const axios = require("axios");
 const port=process.env.PORT || 8000;
 const app=express().use(body_parser.json());
 require('dotenv').config();
+
 const myTocken="mikhel"
 const tocken="EAAKl03DggZBYBABhUAU2tFBnczc17SjeSeEb3PKYQXTuiboGmxMwgn5VAtbbaBinxx3uO1l9U3w9yMYQb3x6GP2qSEZBZB5fKlwZAxKpUvIZBZCb8zLUAtayaaZA8yZBSQXDzLBLQFgaucv0GdrtVhYAZASOgnylV9Vp8rZAKyZALfXKyX7Tc6gcER1SdXzi0GKUxuWpaodzZBrkHAZDZD"
 app.get("/webhook",(req,res)=>{
@@ -36,8 +37,10 @@ else{
 
 app.post("/webhook",(req,res)=>{
 
-    let body_param = req.body;
-
+    const body_param = req.body;
+    //check the incoming webhook message 
+    console.log("Incoming webhook: " + JSON.stringify(body_param));
+   // Validate the webhook
     if(body_param.object){
         console.log(body_param.entry[0].changes[0].value.messages[0]);
         
@@ -45,13 +48,11 @@ app.post("/webhook",(req,res)=>{
             body_param.entry[0].changes[0]&&
              body_param.entry[0].changes[0].value.messages && 
              body_param.entry[0].changes[0].value.messages[0]){
-                console.log("2");
+                
                
                 let phone_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
-                console.log("3");
                 let from =body_param.entry[0].changes[0].value.messages[0].from;
                 let id =body_param.entry[0].changes[0].value.messages[0].id;
-
                 let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
           
        
@@ -60,44 +61,28 @@ app.post("/webhook",(req,res)=>{
         console.log("Message from user : "+msg_body);
         console.log("user contact : "+from);
         console.log("id "+id);
-        console.log(body_param.entry[0].changes[0].value.messages[0].type);
         
-        const{Configuration, OpenAIApi}=require("openai");
-const config = new Configuration({
-    apiKey:"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",
-});
+         axios.post(
+          process.env.WHATSAPP_SEND_MESSAGE_API,
+          {
+            messaging_product: "whatsapp",
+            recipient_type: "individual",
+            to: from,
+            type: "text",
+            text: {
+              preview_url: false,
+              body: reply,
+            },
+          },
+          {
+            headers: {
+              Authorization: "Bearer " + process.env.WHATSAPP_TOKEN,
+            },
+          }
+        );
+       
 
-const openai = new OpenAIApi(config);
-const runPrompt = async() =>{
-    console.log("running prompt");
-    const prompt = "what is chatgpt";
-    const response= await openai.createCompletion({
-        model:"text-davinci-003",
-        prompt: prompt,
-        max_tokens: 20,
-        temperature: 1,
-    });
-    console.log(response.data);
 
-    // let config = {
-    //   method: 'post',
-    //   maxBodyLength: Infinity,
-    //   url: 'https://graph.facebook.com/v16.0/100713606293834/messages',
-    //   headers: { 
-    //     'Content-Type': 'application/json', 
-    //     'Authorization': 'Bearer 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' 
-    //   },
-    //   data : response.data
-    // };
-    
-    // axios.request(config)
-    // .then((response) => {
-    //   console.log(JSON.stringify(response.data));
-    // })
-    // .catch((error) => {
-    //   console.log(error);
-    // });
-};
 
 
       
@@ -182,9 +167,8 @@ const runPrompt = async() =>{
         
       } 
     if(msg_body=="Hii"){
-    //  module.exports.name(phone_no_id, tocken, from, res);
-    runPrompt();
-    }
+     //module.exports.name(phone_no_id, tocken, from, res);
+   }
 
     
    
