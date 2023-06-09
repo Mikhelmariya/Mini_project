1974196aaf43e9072dcb4b37dc5d2d291bc25bf1
Mikhelmariya <mikhela65@gmail.com>
2023-06-09 23:15:08 +0530

    e

--- contoller.js
+++ contoller.js
@@ -7,10 +7,10 @@ const openai = new OpenAIApi(config);
 const runPrompt = async(prompt_message) =>{
     
     
-    const response= await openai.createCompletion({
+    const response= await openai.createChatCompletion({
         model:"gpt-3.5-turbo",
-        messages: [{ role: 'system', content: 'You are a mentor for freshers coming to TKM College of engineering,Kollam.Start with a greeting message . The users  have no idea about college and career opportunities of engineering.They may not have  confidence to try out new things.They may be confused regarding clubs and its associated activities. Neither they  have idea about hackathons or importance of networking. Help students as a mentor to prosper in their career . Answer to all their queries in short but relevant guidance. If you dont understand the question, respond with a apology message . ' }, { role: 'user', content: prompt_message }],
         prompt: prompt_message,
+        messages: [{ role: 'system', content: 'You are a mentor for freshers coming to TKM College of engineering,Kollam.Start with a greeting message . The users  have no idea about college and career opportunities of engineering.They may not have  confidence to try out new things.They may be confused regarding clubs and its associated activities. Neither they  have idea about hackathons or importance of networking. Help students as a mentor to prosper in their career . Answer to all their queries in short but relevant guidance. If you dont understand the question, respond with a apology message . ' }, { role: 'user', content: prompt }],
         max_tokens: 20,
         temperature: 1,
     });
